f178ffd4127024d0818ace6d64207f974eae6541
m.mirjavadi <m.mirjavadi@mediana.ir>
2019-03-09 17:22:29 +0330

    Test redux-saga for asynchronous operation, for instance call web api from client.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import * as actionTypes from "./store/actionTypes";
+import {withStyles} from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import {connect} from "react-redux";
 
+const styles = {
+    card: {
+        minWidth: 50,
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+};
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        const {classes} = this.props;
+        const posts = this.props.posts.map(post => (
+            <Card key={post.id} className={classes.card}>
+                <CardContent>
+                    <Typography className={classes.title} color="textSecondary" gutterBottom>
+                        {post.title}
+                    </Typography>
+                    <Typography className={classes.pos} color="textSecondary">
+                        {post.body}
+                    </Typography>
+                </CardContent>
+            </Card>
+        ));
+
+        return (
+            <div className="App">
+                <button onClick={() => this.props.onIncrement()}> Increment</button>
+                <button onClick={() => this.props.onDecrement()}> Decrement</button>
+                <button onClick={() => this.props.onGetPosts()}> Get Posts</button>
+                <div>
+                    <h1> {this.props.age} </h1>
+                </div>
+                <div>
+                    {posts}
+                </div>
+            </div>
+        );
+    }
 }
 
-export default App;
+const mapsStateToProps = state => {
+    return {
+        age: state.appData.age,
+        posts: state.appData.posts
+    }
+};
+const mapsDispatchToProps = dispatch => {
+    return {
+        onIncrement: () => dispatch(actionTypes.increment()),
+        onDecrement: () => dispatch(actionTypes.decrement()),
+        onGetPosts: () => dispatch(actionTypes.getPosts()),
+    }
+};
+export default connect(mapsStateToProps, mapsDispatchToProps)(withStyles(styles)(App));
--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore, applyMiddleware, compose, combineReducers} from "redux";
+import rootReducer from "./store/reducer";
+import {Provider} from "react-redux";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./store/sagas/saga";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const reducers = combineReducers({
+    appData: rootReducer
+});
+
+const sagaMiddleWare = createSagaMiddleware();
+const store = createStore(reducers, composeEnhancers(applyMiddleware(sagaMiddleWare)));
+sagaMiddleWare.run(rootSaga);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
